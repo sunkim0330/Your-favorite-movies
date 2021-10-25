29a3540b4697af0c8d3e2883e33dcbc78442ca92
Sun Kim <sunkim0205@gmail.com>
2021-10-25 14:27:26 -0500

    final check before submitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const ENTRY = path.join(__dirname, 'src', 'index.js');
 const OUTPUT = path.join(__dirname, 'dist');
